c63a6d5474a2da00f432ad1cc420b8695622b11a
Naiara "Kim <naiaraishiyacademico@gmail.com>
2026-02-27 22:46:58 -0300

    Introdução ao front do TCC

---
+++ src/pages/Sobre/index.jsx
@@ -0,0 +1,8 @@
+export default function Sobre () {
+    return(
+        <>
+            <h1>Sobre</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo accusantium, deserunt soluta, et ratione est nihil harum expedita, culpa molestias iure enim quisquam labore quae saepe possimus excepturi tempora aspernatur!</p>
+        </>
+    )
+}
